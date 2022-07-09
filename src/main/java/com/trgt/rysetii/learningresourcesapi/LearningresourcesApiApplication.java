c99182ee0a7918026639371ca137c3cc385e3463
Harish Sidlaghatta <anirudh.k.harish@gmail.com>
2022-07-10 02:40:44 +0530

    Added an entity and created an object

--- src/main/java/com/trgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
+++ src/main/java/com/trgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -1,13 +1,24 @@
 package com.trgt.rysetii.learningresourcesapi;
 
+import com.trgt.rysetii.learningresourcesapi.entity.LearningResource;
+import com.trgt.rysetii.learningresourcesapi.entity.LearningResourceStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LearningresourcesApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningresourcesApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearningresourcesApiApplication.class, args);
+        LearningResource lr = new LearningResource(1, "Spring Boot Course", 599.0, 699.0,
+                LearningResourceStatus.LIVE,
+                LocalDate.of(2022, 8, 3),
+                LocalDate.of(2022, 10, 4),
+                LocalDate.of(2022, 9, 4));
+
+        System.out.println(lr.getProductName());
+    }
 
 }
---
+++ src/main/java/com/trgt/rysetii/learningresourcesapi/entity/LearningResourceStatus.java
@@ -0,0 +1,8 @@
+package com.trgt.rysetii.learningresourcesapi.entity;
+
+public enum LearningResourceStatus {
+    PLANNING,
+    PUBLISHED,
+    LIVE,
+    RETIRED;
+}
