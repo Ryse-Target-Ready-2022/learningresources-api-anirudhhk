3bb6cb02363ba6e393a70bc493e10af9ccb5bfd6
Harish Sidlaghatta <anirudh.k.harish@gmail.com>
2022-07-10 16:39:06 +0530

    Added method to load data from CSV file

--- src/main/java/com/trgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
+++ src/main/java/com/trgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -2,23 +2,26 @@ package com.trgt.rysetii.learningresourcesapi;
 
 import com.trgt.rysetii.learningresourcesapi.entity.LearningResource;
 import com.trgt.rysetii.learningresourcesapi.entity.LearningResourceStatus;
+import com.trgt.rysetii.learningresourcesapi.service.LearningResourceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class LearningresourcesApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearningresourcesApiApplication.class, args);
-        LearningResource lr = new LearningResource(1, "Spring Boot Course", 599.0, 699.0,
-                LearningResourceStatus.LIVE,
-                LocalDate.of(2022, 8, 3),
-                LocalDate.of(2022, 10, 4),
-                LocalDate.of(2022, 9, 4));
+        LearningResourceService lrs = new LearningResourceService();
+        LearningResource lr = lrs.createLearningResource(
+                new String[]{"1", "Spring Boot Course", "599.0", "699.0", "LIVE", "03-08-2022", "04-10-2022",
+                        "04-09-2022"});
 
-        System.out.println(lr.getProductName());
+        System.out.println(lr.getLearningResourceId() + "\t" + lr.getProductName());
+
     }
 
 }
